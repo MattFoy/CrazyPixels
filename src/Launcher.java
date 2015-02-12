dc31f0a0396cdd5febf87e69bd674466bc604807
MattFoy <mattfoy91@gmail.com>
2015-02-12 02:10:51 -0500

    Minor refactor
    Should make it easier to make this more like a "proper" screensaver and
    to implement J.ini later.

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,13 +9,46 @@ public class Launcher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// http://stackoverflow.com/questions/1936566/how-do-you-get-the-screen-width-in-java
-		GraphicsDevice[] gs = GraphicsEnvironment.getLocalGraphicsEnvironment()
-				.getScreenDevices();
+		if (args.length > 0) {
+			if (args[0] == "/c") {
+				configure();
+				
+				//launch a configuration form?
+			} else if (args[0] == "/p") {
+				preview();
+				
+				//Maybe there's some windows api hook for this, probably not important
+			} else if (args[0] == "/s") {
+				show();
+
+				// http://stackoverflow.com/questions/1936566/how-do-you-get-the-screen-width-in-java
+				
+			}
+		} else {
+			show();
+		}
+
+	}
+	
+	public static void configure() {
+		
+	}
+	
+	public static void preview() {
+		
+	}
+	
+	public static void show() {
+		GraphicsDevice[] gs = GraphicsEnvironment
+				.getLocalGraphicsEnvironment().getScreenDevices();
 		for (int j = 0; j < gs.length; j++) {
 			GraphicsConfiguration[] gc = gs[j].getConfigurations();
 			for (int i = 0; i < gc.length; i++) {
-				boolean splitScreen = false; //this splits each screen into quarters for mini-displays instead
+				boolean splitScreen = false; // this splits each screen
+												// into quarters for
+												// mini-displays instead
+												// of using the whole
+												// screen
 				if (false) {
 					// Launch a canvas for each screen
 					Rectangle r = gc[i].getBounds();
@@ -24,8 +57,9 @@ public class Launcher {
 							r.height / 2);
 					bounds[1] = new Rectangle(r.x + (r.width / 2), r.y,
 							r.width / 2, r.height / 2);
-					bounds[2] = new Rectangle(r.x, r.y + (r.height / 2),
-							r.width / 2, r.height / 2);
+					bounds[2] = new Rectangle(r.x,
+							r.y + (r.height / 2), r.width / 2,
+							r.height / 2);
 					bounds[3] = new Rectangle(r.x + (r.width / 2), r.y
 							+ (r.height / 2), r.width / 2, r.height / 2);
 
@@ -38,7 +72,6 @@ public class Launcher {
 				}
 			}
 		}
-
 	}
 
 	public void buildUI(Rectangle r, int preset, int canvasRatio) {
